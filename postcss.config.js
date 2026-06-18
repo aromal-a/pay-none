62d3a05437a97ca28f7ba8e357189f7dd39e3919
Aromal A <82209054+aromal-a@users.noreply.github.com>
2026-06-18 17:22:47 +0530

    Clean up postcss.config.js - remove corrupted syntax

--- postcss.config.js
+++ postcss.config.js
@@ -1,38 +1,6 @@
 export default {
   plugins: {
-    tailwindcss: {...trailwinds},
-    autoprefixer: {..fixer,..broker},
+    tailwindcss: {},
+    autoprefixer: {},
   },
-    x.j -> Input, oc-specta: <Directives,Casual,reduction,Sickness,Her-Game>
-    y.l - > ouput ,  os-self: casul-int: causative: KODEX: VODEX:  CABINA
-    l.m -0 > Sang-Knots: touch-[mI : CASA - Connectives(
-      eren_ P:  klogf - kopnnectAggent (Agentic:  CI / JOB : Hi " HO - / gormatives: [
-
-
-
-        FontKnote - Moskell : Bisnam : nmainam : kolg :  <Kre: prestx: crepe: [
-
-          CropCatch([..Motatives, Itives: tattives: Klop-Klives, ])
-        ]>
-
-Nava-Lord: [Para-acative-  Vita:  sective:  FEAR ; FORGERENt: FoREFGERy : JAp -= AGENMT,  NAPPERy : smap-sagent]
-      ])Carc k_eth : -=[ewsnamod: [
-                        Kavees: non - sopa:  [Decorative: [Consta: lacquer: logative: iop [
-        Kl : klog: Hi : [name, score- para[Mate: state- regulatory :  reegencies: [
-
-
-  Snorkee: dets:  detsL: [Snorkking: [crap-Krode: cRude- cad: [crwal ;' [Dup :  li : cate: [
-        Ada; []
-        \
-  ]NAka-letter; Porgemnta; [=kleing : decrim "  Osk : lk ": jkwerall ; []
-        
-        Neppelar : truxsciDOM (om-Listener: ebi_sasx:  log: Hr-Tives: p[od:  forms: [
-          info_lactations
-        ])
-          
-\]]]]]
-        ]]]
-                        ]]]
-      ]]
-    )]
 };
