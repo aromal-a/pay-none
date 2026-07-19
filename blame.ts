4b0c4bb5fce3bbc37b0d058f6ed6f99f6119d8d0
Aromal A <82209054+aromal-a@users.noreply.github.com>
2026-07-19 22:57:47 +0530

    Create blame.ts

---
+++ blame.ts
@@ -0,0 +1,17 @@
+Type-Script [Deadass-dealings , _lucky-spin(Redo -  recall , 
+                                          
+                                          Frame ,  j - spins -   []
+                                          off : section - []
+a - connect :  C-PtET [x.conta( , Sart- vet :l <Saarthi.com>)
+b - bonnet  :  auto-no-regain(Batgain , Upper-Tuscx(As-Model))
+c - canula: c: <Fortran(Guest-basics :  [Filings, simplification , Matters = saved() , No-fired ->() ->  Producing matters childrens death is not atomic but fire ans arms racing , 
+  This is primordial and the world is enduring by hiding ,  Not Saved ! Compiled : [Sick-childrenTaken ?  care- immigrators()s:  : Not- firing i-real(oi / dev/self-null , Citizens[] )[]]])>
+ *, count = 'kill ? [''imp
+  Fortrtrt - errer-   { ][ 
+     erative] ,  'cruel' , 'Judge-under-crimes()' . / formbidable-  [/amicable-Consitutions : -  LOver's Dead- Curry : [
+         clo[ud9 = =?> [] Soup-beggining(end : solemn ,  Say-em :  tresicx(..met-tar, soughtful, venture-beggining , adjuncture(..fixing , )))
+         Falled-Winter : flawed cater - [Cut in Slice : [Met - em 'Curves :   [Vuscta , Surveyski] ']]'
+        S-pharm:- [''S-form] , prod-connections('impl - a : connections (,sm-grain ,  Train-Transfer , Good-caol ,  Steam-favoure , Hach-Repacking(Loyaly , Guide_dog)) )
+ ]]' 
+  ]
+                                          )] 
