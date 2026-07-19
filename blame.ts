ad029786ad5ca7f656cf2098a6e372b8deb073ee
Aromal A <82209054+aromal-a@users.noreply.github.com>
2026-07-20 00:33:54 +0530

    Refactor blame.ts for better structure
    Refactor blame.ts to improve structure and readability.

--- blame.ts
+++ blame.ts
@@ -6,12 +6,42 @@ a - connect :  C-PtET [x.conta( , Sart- vet :l <Saarthi.com>)
 b - bonnet  :  auto-no-regain(Batgain , Upper-Tuscx(As-Model))
 c - canula: c: <Fortran(Guest-basics :  [Filings, simplification , Matters = saved() , No-fired ->() ->  Producing matters childrens death is not atomic but fire ans arms racing , 
   This is primordial and the world is enduring by hiding ,  Not Saved ! Compiled : [Sick-childrenTaken ?  care- immigrators()s:  : Not- firing i-real(oi / dev/self-null , Citizens[] )[]]])>
- *, count = 'kill ? [''imp
+ *, count = 'kill ? [''impl
   Fortrtrt - errer-   { ][ 
-     erative] ,  'cruel' , 'Judge-under-crimes()' . / formbidable-  [/amicable-Consitutions : -  LOver's Dead- Curry : [
-         clo[ud9 = =?> [] Soup-beggining(end : solemn ,  Say-em :  tresicx(..met-tar, soughtful, venture-beggining , adjuncture(..fixing , )))
-         Falled-Winter : flawed cater - [Cut in Slice : [Met - em 'Curves :   [Vuscta , Surveyski] ']]'
-        S-pharm:- [''S-form] , prod-connections('impl - a : connections (,sm-grain ,  Train-Transfer , Good-caol ,  Steam-favoure , Hach-Repacking(Loyaly , Guide_dog)) )
- ]]' 
-  ]
-                                          )] 
+     erative] ,  'cruel' , 'Judge-under-crimes()' . / formbidable-  [/amicable-Consitutions : -  LOver's Dead- Curry<STEVENS : STIFF' arms, One-break , One-cancer> : [
+        clo[ud9 = =?> [] Soup-beggining(end : solemn ,  Say-em :  tresicx(..met-tar, soughtful, venture-beggining , adjuncture(..fixing , )))
+        Falled-Winter : flawed cater - [Cut in Slice : [Met - em 'Curves :   [Vuscta , Surveyski] ']]'
+        S-pharm:- [''S-form] , prod-connections('impl - a : connections (,sm-grain ,  Train-Transfer , Good-coal ,  Steam-favouritee , 
+        Hach-Repacking(Loyally , Guide_dog)evil-demo)deamon : unpacking<zip.inductions()> , [I-frame : e-calls, lon-rusk , busk- jaisins])
+ 
+d - catlogs[/-Game :<Fern.Ctx> :: Gang_proofer{solo, 18}]
+e - einstream : Eigen-Form(Eigen : precise : <Boot-Strapping:Time_Stacking:Initials: Vented=> propulsion : return(guilt-due , carbonated-[SS.due(/CUDADrivers => form-c: 'content-art')])>),
+f - -[Set-Triage {Merge:Concord:Tele-lesson(), Tep.dots(#ASCII/art-[Membrane [ON] g , Puter- [Div:  codolor - ['dovlor', /gradient]]])} , ]
+g - hi[hell '0 ' , ice-format{Captured:Tortureres:Torch(Id : Sequencing : {
+  Tensor.load(Safe-Tensing:Time-line:Formattins) [Type + 'Script' , #Id ,  frame-decoration( , [doors, outlocking, B-fold, Varians{S: holds}])]
+  [Tensor.spring(H-arm:Set-Arm:Fret-Grooving) , Di-electional [Tor:int{$: ;find-sub , hidden = dark , units = true}]]
+  ,Outlocking[Matters[S-cam , Frame : [<Jitter> .<Er.com()> [Explicit : <Ivenstat : statenv. env : load().configurations>]
+  ,SetFrockin(Camer,  Scammer ,  Frolicking , Woods,  Bug-fight, Infect:{es.settings()})                                                          
+  ,Founty-gill : Bill:-Fronty :((open-x :  x- cudsi [''Si-framin] ,  male-[Decenting'] , true but not accepting , it is not close to call accepting her nature , 
+                               The divine Maevor Royevour Sreagill , Fulmade[Plaid :  gill()] , [,Haspring, Green-selve.dots(.outphrasing , Para-Transitions)])
+                                MERT_SEIGIN : 'Freet' , 'Doujin' , 'Jin-Overlord' , 'Fin-on-conclords' , 'Feguson-AHH' , 'spacing' , 'spacing' :: <VET-PARASTITE <Stictches>>
+  
+Iven(load : <oscrs : <[New-school , Mut[Ox, os : frame, Lecture{Desc() :: Print("Ev-formed" , 'Mined using children' , 'Lithium-Barrel' , 'shiny-preference between coals')}]]>>)
+Free_arm(.Basics , No-touch - 'hold' , Tight => wret.[wret.length(.same)])
+    Git-Print{Analytics :  Torch-[+X:params : params:[Gtx.[,carvans:lost, outlost{lost :: frame , Restrapped :: Resurgencies}]]}
+        Error("printing tascxm" , vascm() , pending .. , Regulational :  Pr -[basic.dr[Pervix.Drysin()]])
+     
+
+  D'or - [D-ptex :<Font-Net .  Tc[Guide_+name == 'main' , Infer = 'Nonce']>]
+  Flavour , Residular {[Fan.v- index[+c , Crawl-Tuck[Tucke- [T.B]/Fx]?sm-frame]/DPS]/FPS}]]]
+
+  .git-analysis : <m 'commit , casuals() : : Air-spring, [Ari-rang <Daesnrk : {$:Derefences, Fourier[Series (m1,m2,m2 ... Formula)]}>]]>
+  .git, carrers{ensemble.raisins()}
+  cc, uck , :: <Uk : buck> ,  freedom-${expression, recall = fakes, Time- section : 'Parallel-Inducing' = 'frequent'}
+  .gitignore{self[me.apm, pm]} -> Roar By Lesson : [Ceiled by Oppression() , Fan on turbulence , Screen-on-boarding() ]
+  
+})}]
+                                                                                                                                                 
+                                                                                                                                                 
+                                                                                                                                                 
+                                                                                                                                                 ]]'])] 
